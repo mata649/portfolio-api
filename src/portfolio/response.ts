fabe3255ed9668ce5c4792076b63cb8b271592ff
Jose Mata <mata649@hotmail.coma>
2022-11-02 13:14:21 -0600

    response object created

--- src/portfolio/response.ts
+++ src/portfolio/response.ts
@@ -1,8 +1,6 @@
-
-enum ResponseTypes {
+export enum ResponseTypes {
 	CREATED = 201,
 	OK = 200,
-	BAD_REQUEST = 400,
 	RESOURCE_ERROR = 404,
 	SYSTEM_ERROR = 500,
 }
@@ -13,5 +11,3 @@ export class Response {
 		public value: unknown = null
 	) {}
 }
-
-
