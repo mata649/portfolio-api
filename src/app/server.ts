1596845ff659148fd6b6d6df3ec10c0c27d640d7
Jose Mata <mata649@hotmail.coma>
2022-11-02 13:13:54 -0600

    user router added

--- src/app/server.ts
+++ src/app/server.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import { Config } from '../config';
+import userRouter from './routes/user.router';
 
 const app = express();
 const config = Config.loadConfig();
 
 app.use(express.json());
-
-app.listen(config.PORT, ()=>{
-	console.log(`Server running on port ${config.PORT}`)
+app.use('/users', userRouter);
+app.listen(config.PORT, () => {
+	console.log(`Server running on port ${config.PORT}`);
 });
