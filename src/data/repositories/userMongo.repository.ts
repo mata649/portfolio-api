455531d8a9c71518318e3514c92ca7f1a9cbba16
Jose Mata <mata649@hotmail.coma>
2022-11-04 12:52:06 -0600

    repository created

---
+++ src/data/repositories/userMongo.repository.ts
@@ -0,0 +1,30 @@
+import { UserEntity } from '../../domain/entities';
+import { UserRepository } from '../../domain/repositories/user.repository';
+import { UserModel } from '../models';
+
+export class UserMongoRepository implements UserRepository {
+	private userModel = UserModel;
+
+	private createUserEntity = (user: UserEntity): UserEntity => {
+		return {
+			email: user.email,
+			name: user.name,
+			id: user.id,
+			password: user.password,
+		};
+	};
+	async createUser(user: UserEntity): Promise<UserEntity> {
+		const newUser = new this.userModel(user);
+		await newUser.save();
+		return this.createUserEntity(newUser);
+	}
+	async getUserByEmail(email: string): Promise<UserEntity | null> {
+		const userFound = await this.userModel.findOne({ email });
+		userFound?.email;
+		return userFound && this.createUserEntity(userFound);
+	}
+	async getUserById(id: string | undefined): Promise<UserEntity | null> {
+		const userFound = await this.userModel.findById(id);
+		return userFound && this.createUserEntity(userFound);
+	}
+}
