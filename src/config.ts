2381fb163b648b959c9eb48e365350d84805c7b4
Jose Mata <mata649@hotmail.coma>
2022-11-04 12:49:16 -0600

    new params added

--- src/config.ts
+++ src/config.ts
@@ -2,10 +2,16 @@ import * as dotenv from 'dotenv';
 
 export class Config {
 	private static instance: Config;
-	readonly PORT: string | undefined;
+	readonly PORT: string;
+	readonly MONGO_URL: string;
+	readonly DB_NAME: string;
+	readonly JWT_SECRET_SEED: string;
 	private constructor() {
 		dotenv.config();
-		this.PORT = process.env['PORT'];
+		this.PORT = process.env['PORT'] ?? '';
+		this.MONGO_URL = process.env['MONGO_URL'] ?? '';
+		this.DB_NAME = process.env['DB_NAME'] ?? '';
+		this.JWT_SECRET_SEED = process.env['JWT_SECRET_SEED'] ?? '';
 		console.log(this.PORT);
 	}
 
