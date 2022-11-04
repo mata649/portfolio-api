fbe96768a57443ea78ee90ada787ae08d286c2b7
Jose Mata <mata649@hotmail.coma>
2022-11-04 12:51:31 -0600

    user router added

---
+++ src/application/routes/user.router.ts
@@ -0,0 +1,37 @@
+import { Router } from 'express';
+import { check } from 'express-validator';
+import { UserMongoRepository } from '../../data/repositories';
+import { UserUseCase } from '../../domain/useCases';
+import { UserController } from '../controllers';
+import { fieldValidator } from '../middlewares/fieldValidator';
+
+const router = Router();
+const userRepository = new UserMongoRepository();
+const userUseCase = new UserUseCase(userRepository);
+const userCtrl = new UserController(userUseCase);
+
+router.post(
+	'/',
+	check('email')
+		.isEmail()
+		.normalizeEmail()
+		.withMessage('invalid email format'),
+	check('name').notEmpty().withMessage('name is required'),
+	check('password')
+		.isLength({ min: 8 })
+		.withMessage('password require at least 8 characters'),
+	fieldValidator,
+	userCtrl.create
+);
+
+router.post(
+	'/auth',
+	check('email')
+		.isEmail()
+		.normalizeEmail()
+		.withMessage('invalid email format'),
+	check('password').notEmpty(),
+	fieldValidator,
+	userCtrl.login
+);
+export { router as userRouter };
