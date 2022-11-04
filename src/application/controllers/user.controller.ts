b5bd1dc33e3773631027cb7013722be40e5f9b56
Jose Mata <mata649@hotmail.coma>
2022-11-04 12:50:55 -0600

    user controller apparently finished

---
+++ src/application/controllers/user.controller.ts
@@ -0,0 +1,37 @@
+import { Request, Response } from 'express';
+import { CreateUserDto, LoginUserDto, UserEntity } from '../../domain/entities';
+import { ResponseTypes } from '../../domain/response';
+import { UserUseCase } from '../../domain/useCases/user.usecases';
+import { generateJWT } from '../auth/jwt';
+
+export class UserController {
+	constructor(private userUseCase: UserUseCase) {}
+
+	create = async (req: Request, res: Response) => {
+		const user: CreateUserDto = {
+			email: req.body.email,
+			name: req.body.name,
+			password: req.body.password,
+		};
+		const response = await this.userUseCase.create(user);
+
+		res.status(response.type).json(response.value);
+	};
+
+	login = async (req: Request, res: Response) => {
+		const user: LoginUserDto = {
+			email: req.body.email,
+			password: req.body.password,
+		};
+		const response = await this.userUseCase.login(user);
+		if (response.type === ResponseTypes.OK) {
+			const user = response.value as UserEntity;
+			res.status(response.type).json({
+				...user,
+				jwt: await generateJWT(user.id, user.name),
+			});
+		} else {
+			res.status(response.type).json(response.value);
+		}
+	};
+}
