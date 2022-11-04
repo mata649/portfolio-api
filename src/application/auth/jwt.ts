f5ffe44d1450b3329999bbf128025bde67bbe978
Jose Mata <mata649@hotmail.coma>
2022-11-04 12:49:03 -0600

    module added

---
+++ src/application/auth/jwt.ts
@@ -0,0 +1,28 @@
+import { UserEntity } from '../../domain/entities';
+import { Config } from '../../config';
+
+import jwt from 'jsonwebtoken';
+const config = Config.loadConfig();
+export const generateJWT = (id: UserEntity['id'], name: UserEntity['name']) => {
+	return new Promise((resolve, reject) => {
+		const payload = { id, name };
+
+		jwt.sign(
+			payload,
+			config.JWT_SECRET_SEED,
+			{
+				expiresIn: '8000h',
+			},
+			(err, token) => {
+				if (err) {
+					console.error(err);
+					reject('Error generating JWT');
+				}
+
+				resolve(token);
+			}
+		);
+	});
+};
+
+
