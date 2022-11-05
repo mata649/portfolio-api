4117a973bab90e914db6d36126d0ff585c7f71a5
Jose Mata <mata649@hotmail.coma>
2022-11-05 13:16:35 -0600

    name changed

--- src/domain/repositories/user.ts
+++ src/domain/repositories/user.ts
@@ -1,7 +1,7 @@
 import { UserEntity } from '../entities/user';
 
 export interface UserRepository {
-	createUser(user: UserEntity): Promise<UserEntity>;
-	getUserByEmail(email: UserEntity['email']): Promise<UserEntity | null>;
-	getUserById(id: UserEntity['id']): Promise<UserEntity | null>;
+	create(user: UserEntity): Promise<UserEntity>;
+	getByEmail(email: UserEntity['email']): Promise<UserEntity | null>;
+	getById(id: UserEntity['id']): Promise<UserEntity | null>;
 }
