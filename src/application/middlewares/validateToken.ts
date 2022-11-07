97b6f33178a2392f878f2d9efdb68f2c501756e2
Jose Mata <mata649@hotmail.coma>
2022-11-06 21:22:41 -0600

    middleware added

---
+++ src/application/middlewares/validateToken.ts
@@ -0,0 +1,38 @@
+import { NextFunction, Request, Response } from 'express';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import { Config } from 'config';
+
+export const validateToken = (
+	req: Request,
+	res: Response,
+	next: NextFunction
+): Response | void => {
+	const token = req.header('Authorization');
+
+	const config = Config.loadConfig();
+	if (!token) {
+		return res.status(401).json({
+			ok: false,
+			msg: 'No token in the request',
+		});
+	}
+
+	try {
+		const { id, name }: JwtPayload = jwt.verify(
+			token,
+			config.JWT_SECRET_SEED
+		) as JwtPayload;
+
+		req.currentUser = {
+			id,
+			name,
+		};
+	} catch (error) {
+		return res.status(401).json({
+			ok: false,
+			msg: 'Token is not valid',
+		});
+	}
+
+	next();
+};
