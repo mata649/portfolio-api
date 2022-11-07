cf3342c98174823bd65f80da585630b91bf02fed
Jose Mata <mata649@hotmail.coma>
2022-11-06 21:36:23 -0600

    skill router added

--- src/application/server.ts
+++ src/application/server.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import { Config } from 'config';
 import { dbConnection } from 'data/mongodb/db/mongoInit';
-import { userRouter } from 'application/routes';
+import { userRouter, skillRouter } from 'application/routes';
 
 dbConnection();
 
@@ -9,6 +9,7 @@ const app = express();
 const config = Config.loadConfig();
 app.use(express.json());
 app.use('/users', userRouter);
+app.use('/skills', skillRouter);
 app.listen(config.PORT, () => {
 	console.log(`Server running on port ${config.PORT}`);
 });
