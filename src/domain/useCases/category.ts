62a5f2bf7321caec4fd0d71dd1f9ea164bae47e4
Jose Mata <mata649@hotmail.coma>
2022-11-06 21:46:54 -0600

    category useCases created

---
+++ src/domain/useCases/category.ts
@@ -0,0 +1,10 @@
+import { createCategoryEntity, CategoryEntity } from 'domain/entities/category';
+import { CategoryRepository } from 'domain/repositories';
+
+import { BaseUseCase } from './baseUseCase';
+
+export class SkillUseCase extends BaseUseCase<CategoryEntity> {
+	constructor(categoryRepository: CategoryRepository) {
+		super(categoryRepository, createCategoryEntity, 'category');
+	}
+}
