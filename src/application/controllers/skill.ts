d6facb7387c9ebf60b87f3dc46cc5c6ca6e078d3
Jose Mata <mata649@hotmail.coma>
2022-11-06 22:40:22 -0600

    now extends from BaseController

--- src/application/controllers/skill.ts
+++ src/application/controllers/skill.ts
@@ -1,41 +1,10 @@
-import { Request, Response } from 'express';
-import { createSkillEntity } from 'domain/entities/skill';
+import { createSkillEntity, SkillEntity } from 'domain/entities/skill';
 import { SkillUseCase } from 'domain/useCases';
+import { BaseController } from './baseController';
 
-export class SkillController {
-	constructor(private skillUseCase: SkillUseCase) {}
 
-	create = async (req: Request, res: Response) => {
-		const skill = createSkillEntity({
-			color: req.body.color,
-			name: req.body.name,
-		});
-		const response = await this.skillUseCase.create(skill);
-
-		res.status(response.type).json(response.value);
-	};
-
-	update = async (req: Request, res: Response) => {
-		const skill = createSkillEntity({
-			id: req.params.id,
-			color: req.body.color,
-			name: req.body.name,
-		});
-		const response = await this.skillUseCase.update(skill);
-		res.status(response.type).json(response.value);
-	};
-
-	getById = async (req: Request, res: Response) => {
-		const response = await this.skillUseCase.getById(req.params.id);
-		res.status(response.type).json(response.value);
-	};
-
-	get = async (_: Request, res: Response) => {
-		const response = await this.skillUseCase.get();
-		res.status(response.type).json(response.value);
-	};
-	delete = async (req: Request, res: Response) => {
-		const response = await this.skillUseCase.delete(req.params.id);
-		res.status(response.type).json(response.value);
-	};
+export class SkillController extends BaseController<SkillEntity> {
+	constructor(skillUseCase: SkillUseCase) {
+		super(skillUseCase, createSkillEntity);
+	}
 }
