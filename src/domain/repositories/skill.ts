dd1873e36802ea92fa94fbc49b36e1a39afcc195
Jose Mata <mata649@hotmail.coma>
2022-11-06 21:37:46 -0600

    skill repository created

--- src/domain/repositories/skill.ts
+++ src/domain/repositories/skill.ts
@@ -1,9 +1,3 @@
 import { SkillEntity } from 'domain/entities/skill';
-
-export interface SkillRepository {
-	create(skill: SkillEntity): Promise<SkillEntity>;
-	get(): Promise<SkillEntity[] | null>;
-	getById(id: SkillEntity['id']): Promise<SkillEntity | null>;
-	delete(id: SkillEntity['id']): Promise<SkillEntity>;
-	update(skill: SkillEntity): Promise<SkillEntity>;
-}
+import { BaseRepository } from './baseRepository';
+export interface SkillRepository extends BaseRepository<SkillEntity> {}
