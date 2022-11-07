f7d448b4e94df6121c099149945ee39b35428cf5
Jose Mata <mata649@hotmail.coma>
2022-11-06 21:37:38 -0600

    now implements SkillRepository

--- src/data/mongodb/repositories/skill.ts
+++ src/data/mongodb/repositories/skill.ts
@@ -1,8 +1,12 @@
 import { baseMongoRepository } from './baseMongoRepository';
 import { SkillEntity, createSkillEntity } from 'domain/entities/skill';
 import { SkillModel } from 'data/mongodb/models/Skill';
+import { SkillRepository } from 'domain/repositories';
 
-export class SkillMongoRepository extends baseMongoRepository<SkillEntity> {
+export class SkillMongoRepository
+	extends baseMongoRepository<SkillEntity>
+	implements SkillRepository
+{
 	constructor() {
 		super(SkillModel, createSkillEntity);
 	}
