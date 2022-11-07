20259a4dfd4c3e032dd9855aad83f0ac6422f447
Jose Mata <mata649@hotmail.coma>
2022-11-06 22:21:57 -0600

    base controller added bcz generics are cool

---
+++ src/application/controllers/baseController.ts
@@ -0,0 +1,36 @@
+import { Request, Response } from 'express';
+import { BaseUseCase } from 'domain/useCases/baseUseCase';
+
+export class BaseController<T extends { id?: string }> {
+	constructor(
+		private baseUseCase: BaseUseCase<T>,
+		private createItemEntity: (item: Partial<T>) => T
+	) {}
+
+	create = async (req: Request, res: Response) => {
+		const item = this.createItemEntity(req.body);
+		const response = await this.baseUseCase.create(item);
+
+		res.status(response.type).json(response.value);
+	};
+
+	update = async (req: Request, res: Response) => {
+		const item = this.createItemEntity({ ...req.body, id: req.params.id });
+		const response = await this.baseUseCase.update(item);
+		res.status(response.type).json(response.value);
+	};
+
+	getById = async (req: Request, res: Response) => {
+		const response = await this.baseUseCase.getById(req.params.id);
+		res.status(response.type).json(response.value);
+	};
+
+	get = async (_: Request, res: Response) => {
+		const response = await this.baseUseCase.get();
+		res.status(response.type).json(response.value);
+	};
+	delete = async (req: Request, res: Response) => {
+		const response = await this.baseUseCase.delete(req.params.id);
+		res.status(response.type).json(response.value);
+	};
+}
