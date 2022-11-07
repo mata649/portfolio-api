c023cdf286ecfc5cc80659f60d78f807db8ea70c
Jose Mata <mata649@hotmail.coma>
2022-11-07 17:39:38 -0600

    filters support added

--- src/domain/repositories/baseRepository.ts
+++ src/domain/repositories/baseRepository.ts
@@ -1,6 +1,6 @@
 export interface BaseRepository<T extends { id?: string }> {
 	create(item: T): Promise<T>;
-	get(): Promise<T[] | null>;
+	get(filters: Partial<T>): Promise<T[] | null>;
 	getById(id: T['id']): Promise<T | null>;
 	delete(id: T['id']): Promise<T>;
 	update(skill: T): Promise<T>;
