15c86994b5081cfa5ee7bf117e2138db044866dc
Jose Mata <mata649@hotmail.coma>
2022-11-06 22:40:39 -0600

    repository created

---
+++ src/data/mongodb/repositories/category.ts
@@ -0,0 +1,13 @@
+import { baseMongoRepository } from './baseMongoRepository';
+import { CategoryEntity, createCategoryEntity } from 'domain/entities/category';
+import { CategoryModel } from 'data/mongodb/models/';
+import { CategoryRepository } from 'domain/repositories';
+
+export class CategoryMongoRepository
+	extends baseMongoRepository<CategoryEntity>
+	implements CategoryRepository
+{
+	constructor() {
+		super(CategoryModel, createCategoryEntity);
+	}
+}
