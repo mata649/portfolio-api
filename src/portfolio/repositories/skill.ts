bceff0a60b803ab25e5aaebebef3d9919050a3f7
Jose Mata <mata649@hotmail.coma>
2022-11-17 17:26:52 -0600

    getSkillsByCategory added

--- src/portfolio/repositories/skill.ts
+++ src/portfolio/repositories/skill.ts
@@ -1,7 +1,9 @@
-import {  SkillEntity } from 'portfolio/entities';
+import { CategoryEntity, SkillEntity } from 'portfolio/entities';
 import { BaseRepository } from './baseRepository';
-
+export interface SkillByCategory extends Omit<CategoryEntity,'id'> {
+	skills: Omit<SkillEntity, 'idCategory'|'id'>[];
+}
 
 export interface SkillRepository extends BaseRepository<SkillEntity> {
-
+	getSkillsByCategory(): Promise<SkillByCategory[] | null>;
 }
