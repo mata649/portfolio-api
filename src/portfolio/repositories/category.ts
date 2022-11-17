959e84be1f9f11e52da95b8ce854c65c368ffdc4
Jose Mata <mata649@hotmail.coma>
2022-11-17 17:27:04 -0600

    idk

--- src/portfolio/repositories/category.ts
+++ src/portfolio/repositories/category.ts
@@ -1,3 +1,6 @@
 import { CategoryEntity } from 'portfolio/entities';
 import { BaseRepository } from './baseRepository';
-export interface CategoryRepository extends BaseRepository<CategoryEntity> {}
+
+export interface CategoryRepository extends BaseRepository<CategoryEntity> {
+
+}
