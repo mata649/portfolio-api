4d64fa44a28f0ccc8d343dd16276e3a8bc1b2c42
Jose Mata <mata649@hotmail.coma>
2022-11-18 02:13:56 -0600

    a

--- src/data/mongodb/repositories/project.ts
+++ src/data/mongodb/repositories/project.ts
@@ -2,7 +2,6 @@ import { baseMongoRepository } from './baseMongoRepository';
 import { ProjectEntity, createProjectEntity } from 'portfolio/entities';
 import { ProjectModel } from 'data/mongodb/models/';
 import { ProjectRepository } from 'portfolio/repositories';
-import { Filters } from 'portfolio/repositories/baseRepository';
 
 export class ProjectMongoRepository
 	extends baseMongoRepository<ProjectEntity>
