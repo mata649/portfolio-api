2aa067dbfb55fa7bc49bba8d8a7a8ba9f6c06cf8
Jose Mata <mata649@hotmail.coma>
2022-11-18 02:13:37 -0600

    projectsByCategoryDeleted

--- src/data/mongodb/repositories/project.ts
+++ src/data/mongodb/repositories/project.ts
@@ -1,7 +1,7 @@
 import { baseMongoRepository } from './baseMongoRepository';
 import { ProjectEntity, createProjectEntity } from 'portfolio/entities';
 import { ProjectModel } from 'data/mongodb/models/';
-import { ProjectRepository, ProjectsByCategory } from 'portfolio/repositories';
+import { ProjectRepository } from 'portfolio/repositories';
 import { Filters } from 'portfolio/repositories/baseRepository';
 
 export class ProjectMongoRepository
@@ -11,8 +11,5 @@ export class ProjectMongoRepository
 	constructor() {
 		super(ProjectModel, createProjectEntity);
 	}
-	getProjectsByCategory(filters: Filters<string>): Promise<ProjectsByCategory[] | null> {
-		console.log(filters)
-		throw new Error('Method not implemented.');
-	}
+
 }
