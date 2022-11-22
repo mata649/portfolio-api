962083e7c0ca642fb919ae9c8d24a9e533a876ec
Jose Mata <mata649@hotmail.coma>
2022-11-21 21:44:18 -0600

    added cors support

--- src/rest/server.ts
+++ src/rest/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors'
 import { Config } from 'config';
 import { dbConnection } from 'data/mongodb/db/mongoInit';
 import {
@@ -13,6 +14,7 @@ import {
 dbConnection();
 
 const app = express();
+app.use(cors())
 const config = Config.loadConfig();
 app.use(express.json());
 app.use('/users', userRouter);
