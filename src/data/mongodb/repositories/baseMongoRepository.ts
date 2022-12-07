5dddd5ba5cc36e74996953476c18113983d89821
Jose Mata <mata649@hotmail.coma>
2022-12-06 20:11:39 -0600

    imports changed

--- src/data/mongodb/repositories/baseMongoRepository.ts
+++ src/data/mongodb/repositories/baseMongoRepository.ts
@@ -1,6 +1,9 @@
-import { BaseRepository, Filters, Results } from 'portfolio/repositories/baseRepository';
-import { Model, Document, HydratedDocument } from 'mongoose';
-
+import {
+	BaseRepository,
+	Filters,
+	Results,
+} from 'portfolio/repositories/baseRepository';
+import { Model, Document, HydratedDocument, SortOrder } from 'mongoose';
 
 export class baseMongoRepository<T extends { id?: string }>
 	implements BaseRepository<T>
@@ -32,23 +35,46 @@ export class baseMongoRepository<T extends { id?: string }>
 		}
 		return filters;
 	}
+
+	protected parseOrderBy(orderBy: Filters<T>['orderBy']) {
+		let args: [string, SortOrder][] = [];
+		if (orderBy.includes(',')) {
+			const params = orderBy.split(',');
+			for (const param of params) {
+				const arg = param.split('_');
+				if (arg.length === 2) {
+					args.push([arg[0], arg[1] === 'desc' ? -1 : 1]);
+				}
+			}
+			return args;
+		}
+		const arg = orderBy.split('_');
+		if (arg.length === 2) {
+			args.push([arg[0], arg[1] === 'desc' ? -1 : 1]);
+		}
+		return args;
+	}
 	async create(item: T): Promise<T> {
 		const itemCreated = new this.model(item);
 		await itemCreated.save();
 		return this.createItemEntity(itemCreated);
 	}
 	async get(filters: Filters<T>): Promise<Results<T> | null> {
-
 		filters.filters = this.parseFilters(filters.filters);
+		const orderBy = this.parseOrderBy(filters.orderBy);
+
 		const itemsFound = await this.model
 			.find(filters.filters)
+			.sort(orderBy)
+			.collation({ locale: 'en', caseLevel: true })
 			.limit(filters.limit * 1)
 			.skip((filters.page - 1) * filters.limit);
-		const count = await this.model.find(filters.filters).countDocuments()
+
+		const count = await this.model.find(filters.filters).countDocuments();
 		return {
-			data:itemsFound.map((item) => this.createItemEntity(item)),
-			currentPage:filters.page,
-			totalPages: Math.ceil(count / filters.limit)
+			data: itemsFound.map((item) => this.createItemEntity(item)),
+			currentPage: filters.page,
+			totalPages: Math.ceil(count / filters.limit),
 		};
 	}
 	async getById(id: T['id']): Promise<T | null> {
