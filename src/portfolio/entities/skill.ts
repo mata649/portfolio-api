0feec24d19441253ece92278adb88ef08ef78ccd
Jose Mata <mata649@hotmail.coma>
2022-12-06 20:11:52 -0600

    color deleted

--- src/portfolio/entities/skill.ts
+++ src/portfolio/entities/skill.ts
@@ -1,19 +1,16 @@
 export interface SkillEntity {
 	id: string;
 	name: string;
-	color: string;
 	idCategory: string;
 }
 export const createSkillEntity = ({
 	id = '',
 	name = '',
-	color = '',
 	idCategory = '',
 }: Partial<SkillEntity>): SkillEntity => {
 	return {
 		id,
 		name,
-		color,
 		idCategory
 	};
 };
