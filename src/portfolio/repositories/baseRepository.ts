d1e87e4b7b18df5a210c746c9f251985e3acecf5
Jose Mata <mata649@hotmail.coma>
2022-12-30 21:52:00 -0600

    base repository documented

--- src/portfolio/repositories/baseRepository.ts
+++ src/portfolio/repositories/baseRepository.ts
@@ -1,34 +1,87 @@
-export interface OrderBy{
-	by:string,
-	order: 'asc' | 'desc'
+/**Interface representing the order of the elements */
+export interface OrderBy {
+	/**Argument by which elements are ordered*/
+	by: string;
+	/**Order way, ascendant `asc` or descendant `desc`*/
+	order: 'asc' | 'desc';
 }
+/** Interface representing the filters in a request
+ * @typeParam T - Type of properties accepted by the filter
+ */
 export interface Filters<T> {
+	/** Filters of the request */
 	filters: Partial<T>;
+	/** Limit of elements returned */
 	limit: number;
+	/** Page number*/
 	page: number;
-	orderBy: OrderBy[]
+	/**Order of the elements*/
+	orderBy: OrderBy[];
 }
 
+/**
+ * Interface representing the results of a request
+ * @typeParam T - Type of object the data contains
+ */
 export interface Results<T> {
+	//** Array of elements */
 	data: T[];
+	//** The current page*/
 	currentPage: number;
+	//** Total pages */
 	totalPages: number;
 }
+/**
+ * Interface representing the common methods needed by any repository
+ * @typeParam T- Type of object used in the methods
+ */
 export interface BaseRepository<T extends { id?: string }> {
+	/**
+	 * Creates a new element in the repository
+	 * @param item - Element to be created in the repository
+	 * @returns The element created in the repository
+	 */
 	create(item: T): Promise<T>;
-	get(filters: Filters<T>): Promise<Results<T> | null>;
+	/**
+	 * Gets a list of elements from the repository
+	 * @param filters - Filters to be used in the request to the repository
+	 * @returns `Result` with the elements gotten from the repository,the total pages
+	 * and the current page.
+	 */
+	get(filters: Filters<T>): Promise<Results<T>>;
+	/**
+	 * Gets a element by id from the repository
+	 * @param id - identifier of the element to get
+	 * @returns If an element was found, returns element else returns a null
+	 */
 	getById(id: T['id']): Promise<T | null>;
+	/**
+	 * Deletes a element by id from the repository
+	 * @param id - identifier of the element to delete
+	 * @returns If an element was deleted, returns element else returns a null
+	 */
 	delete(id: T['id']): Promise<T | null>;
+	/**
+	 * Updates an element in the repository
+	 * @param item - Element to be created in the repository
+	 * @returns If an element was updated, returns element else returns a null
+	 */
 	update(skill: T): Promise<T>;
 }
 
+/**
+ * Returns a new `Filters` object, setting proper values to
+ * the undefined values
+ * @param item - The object to create the new `Filters` object
+ * @returns The new `Filters` object
+ *
+ */
 export const createFilters = <T>({
 	filters = {},
 	limit = 10,
 	page = 1,
-	orderBy = []
+	orderBy = [],
 }: Partial<Filters<T>>): Filters<T> => {
-
 	if (isNaN(limit)) {
 		limit = 10;
 	}
@@ -39,6 +92,6 @@ export const createFilters = <T>({
 		filters,
 		limit,
 		page,
-		orderBy
+		orderBy,
 	};
 };
